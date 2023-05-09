5b52e1c559748ad47804c4526f3cd0430c175acf
teoyt <125856128+teoyt@users.noreply.github.com>
2023-05-09 20:55:09 +0800

    Update Cart.aspx.cs
    hi

--- Web_Assignment/Web_Assignment/Cart.aspx.cs
+++ Web_Assignment/Web_Assignment/Cart.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Security;
 
 namespace Web_Assignment
 {
@@ -12,6 +15,32 @@ namespace Web_Assignment
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            //String email = Gmail.Text;
+            //String password = Password.Text;
+            SqlConnection con;
+            string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            con = new SqlConnection(strCon);
+            con.Open();
+            string query = "SELECT COUNT(*) FROM [User] WHERE Email=@email AND Password=@Password";
+
+            SqlCommand cmdInsertUser = new SqlCommand(query, con);
+
+           // cmdInsertUser.Parameters.AddWithValue("@email", email);
+            //cmdInsertUser.Parameters.AddWithValue("@password", password);
+            int count = Convert.ToInt32(cmdInsertUser.ExecuteScalar());
+            cmdInsertUser.ExecuteNonQuery();
+
+            if (count == 1)
+            {
+                // User authenticated successfully, redirect to a different page
+                FormsAuthentication.SetAuthCookie("testauth", false);
+                Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                // Invalid credentials
+                Response.Redirect("Register.aspx");
+            }
         }
     }
 }
