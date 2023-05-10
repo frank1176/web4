e9bb4dafd5eddab983c9d59f5c1d38efda79311d
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-05-10 16:25:29 +0800

    testing

--- Web_Assignment/Web_Assignment/Register.aspx.designer.cs
+++ Web_Assignment/Web_Assignment/Register.aspx.designer.cs
@@ -213,12 +213,21 @@ namespace Web_Assignment
         protected global::System.Web.UI.WebControls.RequiredFieldValidator AnswerRequired;
 
         /// <summary>
-        /// login control.
+        /// btnRegister control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button login;
+        protected global::System.Web.UI.WebControls.Button btnRegister;
+
+        /// <summary>
+        /// lbRegister control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbRegister;
     }
 }
